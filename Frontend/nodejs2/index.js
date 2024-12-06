6304c883090a8d35b8433d5e05322be7299c0825
kisric519 <kisric519@hengersor.hu>
2024-12-06 10:33:44 +0100

    node calculator

--- Frontend/nodejs2/index.js
+++ Frontend/nodejs2/index.js
@@ -1,7 +1,11 @@
 // wrapper function
-// function(__dirname, __filename) {
+// function(__dirname, __filename, require) {
 
-console.log(__dirname)
-console.log(__filename)
+console.log(__dirname);
+console.log(__filename);
+
+add = require('./calculator.js');
+
+console.log(add(2,3));
 
 // }
