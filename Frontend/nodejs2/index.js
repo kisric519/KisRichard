019de876ededa5c919d66b348a4c457efcaa1950
kisric519 <kisric519@hengersor.hu>
2024-12-06 10:59:09 +0100

    node server

--- Frontend/nodejs2/index.js
+++ Frontend/nodejs2/index.js
@@ -5,7 +5,15 @@ console.log(__dirname);
 console.log(__filename);
 
 add = require('./calculator.js');
-
 console.log(add(2,3));
+
+
+const http = require('http');
+
+const server = http.createServer(function (req, res) {
+    res.writeHead(200);
+    res.end('Hello World');
+})
+server.listen(8080);
 
 // }
