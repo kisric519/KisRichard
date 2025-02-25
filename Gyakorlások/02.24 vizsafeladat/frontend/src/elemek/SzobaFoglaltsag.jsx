588ab2becb733b469a80210ddd1f83d1ea5badd1
kisric519 <kisric519@hengersor.hu>
2025-02-25 12:06:15 +0100

    vizsga gyak befejezés

--- Gyakorlások/02.24 vizsafeladat/frontend/src/elemek/SzobaFoglaltsag.jsx
+++ Gyakorlások/02.24 vizsafeladat/frontend/src/elemek/SzobaFoglaltsag.jsx
@@ -4,14 +4,27 @@ import axios from 'axios'
 import Szobak from "./Szobak";
 
 const SzobaFoglaltsag = (({szobaid}) => {
-    const [szobak, setSzobak] = useState([]);
+    const [szobafoglalasok, setSzobaFoglalasok] = useState([]);
+
+    function datum(dateString) {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+      
+        return `${year}-${month}-${day}`;
+      }
+      
 
     useEffect(() => {
-        axios.get(`http://127.0.0.1:3001/szoba/`+szobaid)
+        if(szobaid){
+            axios.get('http://127.0.0.1:3001/szobafoglaltsag/'+szobaid)
             .then(res => {
-                setSzobak(res.data)
+                setSzobaFoglalasok(res.data)
+                console.log(res.data)
             })
             .catch(err => console.error(err));
+        }
     }, [szobaid]);
 
     if(szobaid == ""){
@@ -28,7 +41,26 @@ const SzobaFoglaltsag = (({szobaid}) => {
             <h2>Szoba adatai</h2>
             <Szobak id={szobaid}/>
             <br />
-            <h2>Szoba foglaltásga:</h2>
+            <h2>Szoba foglaltsága:</h2>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Szoba neve</th>
+                        <th scope="col">Érkezés dátuma</th>
+                        <th scope="col">Távozás dátuma</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {szobafoglalasok.map(foglalas => (
+                        <tr key={foglalas.id}>
+                            <td>{foglalas.vnev}</td>
+                            <td>{datum(foglalas.erk)}</td>
+                            <td>{datum(foglalas.tav)}</td>
+                        </tr>
+
+                    ))}
+                </tbody>
+            </table>
         </>
     )
 })
